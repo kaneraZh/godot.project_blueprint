feb62edcaf5ebedeadf833f3b4260c87ccfd7ac0
ariel <arielferrada@proton.me>
2024-09-29 04:00:37 -0300

    deleted everything :3

--- scripts/controller/threshold/threshold_normal.gd
+++
@@ -1,7 +0,0 @@
-extends Threshold
-class_name ThresholdNormal
-@export_range(-1.0, 1.0) var value := 0.0
-func test(val:float)->int:
-	return _process_result(
-		clampi( signi(abs(val)-abs(value)), -get_on_below(), get_on_above())
-	)
